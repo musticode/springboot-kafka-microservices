b346f903d02288843467478ae1722802f16a3947
karatasm <mustafa.karatas+gerrit@dataroid.com>
2023-07-23 20:46:10 +0300

    email-service is updated, kafka listener is added

---
+++ email-service/src/main/java/com/example/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,27 @@
+package com.example.emailservice.kafka;
+
+import com.example.basedomains.payload.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(OrderEvent orderEvent){
+        LOGGER.info(String.format("Order event is received %s", orderEvent.toString()));
+
+        System.out.println(orderEvent.getOrder().getOrderId());
+        System.out.println(orderEvent.getOrder().getName());
+        System.out.println(orderEvent.getOrder().getQty());
+    }
+
+}
